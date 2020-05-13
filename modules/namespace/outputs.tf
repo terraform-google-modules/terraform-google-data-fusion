b8ec9f7a4064fd3fbc7d37ae3dbd81aab93c6008
Svetak Sundhar <svetaksundhar@google.com>
2020-05-13 12:37:08 -0400

    Update outputs.tf

--- modules/namespace/outputs.tf
+++ modules/namespace/outputs.tf
@@ -15,6 +15,12 @@
  */
 
 output "name" {
-  value       = cdap_namespace.parent.name
+  value       = cdap_namespace.namespace.name
   description = "The created CDAP namespace"
 }
+
+output "preferences" {
+  value       = cdap_namespace_preferences.preferences
+  description = "The preferences set in the CDAP namespace"
+}
+
